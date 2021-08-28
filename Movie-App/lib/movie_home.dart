f597e58a36d32b1092130de1f189d4343c121df5
Simran Pattnaik <72657303+Simran1604@users.noreply.github.com>
2021-08-28 23:00:03 +0530

    Update movie_home.dart
    Updated homepage

--- Movie-App/lib/movie_home.dart
+++ Movie-App/lib/movie_home.dart
@@ -6,7 +6,9 @@ import 'package:flutter/rendering.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 
-import 'movie-info.dart';
+import 'package:movies/movie_info.dart';
+
+import 'movie_detail.dart';
 final List<Info> infoList=Info.getinfo();
 class movies extends StatelessWidget{
   
@@ -34,7 +36,7 @@ class movies extends StatelessWidget{
             child:Container(
               margin: EdgeInsets.only(left:80),
               width: MediaQuery.of(context).size.width,
-              height: 120,
+              height: 180,
               child: Card(
                 color:Colors.grey.shade900,
                 child:Padding(
@@ -51,17 +53,17 @@ class movies extends StatelessWidget{
                              child:Text(infoList[index].title,
                             style: TextStyle(
                               color:Colors.white,
-                              fontSize: 16,
+                              fontSize: 20,
                               fontWeight: FontWeight.bold,
                             ),),
                           ),
                           
                            Expanded(
                              
-                              child: Text("Rating :${infoList[index].imdbRating}/10",
+                              child: Text("Rating: ${infoList[index].imdbRating}/10",
                       style: TextStyle(
                         color:Colors.grey.shade500,
-                        fontSize: 12,
+                        fontSize: 14,
                         fontWeight: FontWeight.bold,
                       ),),
                             ),
@@ -71,27 +73,31 @@ class movies extends StatelessWidget{
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children:[
-                         Expanded(
-                            child:Text("Released:${infoList[index].released}",
+                        children: [
+                          Expanded(
+                            child:Text("Year: ${infoList[index].year}",
                             style: TextStyle(
                               color:Colors.grey.shade500,
                               fontWeight: FontWeight.bold,
-                              fontSize: 12
+                              fontSize: 14
                             ),),
                          ),
-                          
                            Expanded(
-                              child: Text("${infoList[index].runtime}",style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color:Colors.grey.shade500,
-                                fontSize: 12
-                              ),),
-                           ),
+                                child: Text("Duration:${infoList[index].runtime}",style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color:Colors.grey.shade500,
+                                  fontSize: 14
+                                ),),
+                             ),
+                        ],
+                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children:[
                           Expanded(
-                            child: Text("${infoList[index].rated}",style: TextStyle(
+                            child: Text("Genre: ${infoList[index].genre}",style: TextStyle(
                               fontWeight: FontWeight.bold,
-                              fontSize: 12,
+                              fontSize: 14,
                               color:Colors.grey.shade500,
                             ),),
                           )
@@ -108,30 +114,43 @@ class movies extends StatelessWidget{
         onTap: (){
           Navigator.push(context, MaterialPageRoute(builder: (context)=>MovieDetails(index:i)));
         }
-         ,  );
+           );
 
         }
         
           
 
-          Widget movieImage (String image){
-            return Container(
-              margin: EdgeInsets.only(top:10,left:20),
-              width:100,
-              height:100,
-              decoration: BoxDecoration(
-                shape:BoxShape.circle,
-                image:DecorationImage(image:NetworkImage(image),
-                fit: BoxFit.fill
-                )
-              ),
-            );
+          Widget movieImage (String image,int index){
+             return InkWell(
+               child: Container(
+                  margin: EdgeInsets.only(left:10,right:10,top:20),
+                  width:135,
+                  height:135,
+                  decoration: BoxDecoration(
+                    shape:BoxShape.circle,
+                    border: Border.all(color: Colors.grey.shade900,
+                    width: 3),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 5
+                      )
+                    ],
+                    image:DecorationImage(image:NetworkImage(image),
+                    fit: BoxFit.cover
+                    )
+                  ),
+                ),
+               onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>MovieDetails(index:i)));
+        }
+             );
           }
 
           return Stack(
             children: [
-              movieCard(infoList[index],context),
-              movieImage(infoList[index].images[0]),
+             movieCard(infoList[index],context),
+              movieImage(infoList[index].images[1],index)
               ]);
         
       }),
@@ -139,331 +158,3 @@ class movies extends StatelessWidget{
   }
 }
 
-class MovieDetails extends StatelessWidget {
-  final List<Info> infoList=Info.getinfo();
-
-   MovieDetails({ Key? key, required this.index}) : super(key: key);
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey.shade800,
-      appBar: AppBar(
-        title:Text("Movies"),
-        backgroundColor: Colors.blueGrey.shade900,
-      ),
-
-      body: ListView(
-        
-          children:[
-            Thumbnail(thumbnail: infoList[index].images[0],index: index,),
-            Row(
-              children: [
-                Expanded(child:HeaderWithPoster(index:index)),
-              ],
-            ),
-             Line(),
-            Row(
-              children: [
-                Expanded(child: Details(index: index)),
-              ],
-            ),
-            Line(),
-            Images(posters: infoList[index].images),
-          ]  
-    ),
-    );
-    
-  }
-}
-
-class Thumbnail extends StatelessWidget {
-  final String thumbnail;
-  final int index;
-
-  const Thumbnail({ Key? key, required this.thumbnail, required this.index }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              width:MediaQuery.of(context).size.width,
-              height:180,
-              decoration: BoxDecoration(
-                image: DecorationImage(image: NetworkImage(thumbnail),
-                fit: BoxFit.cover)
-              ),
-            ),
-            //
-            GestureDetector(
-               onTap:()
-            {
-              button(index);
-            } ,
-            child:Icon(Icons.play_circle_outline, size:60,color: Colors.white,
-           ) ,
-            )
-             
-           // IconButton(onPressed: button(index), icon: Icon(Icons.play_circle_outline,size: 60,color: Colors.white,))
-            
-          ],
-         
-        ),
-         Container(
-          decoration:BoxDecoration(
-            gradient:LinearGradient(colors: [Color(0x00f5f5f5),Color(0xfff5f5f5)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter
-            )
-          ),
-          height: 40,
-        )
-      ],      
-    );
-  }
-  button(int index) async
-  {
-    var url=infoList[index].play;
-     if(await canLaunch(url))
-     await launch(url);
-  }
-} 
-
-class HeaderWithPoster extends StatelessWidget {
-  final index;
-  const HeaderWithPoster({ Key? key, this.index }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal:8.0),
-      child: Row(
-        children: [
-          Poster(poster:infoList[index].images[1].toString()),
-          SizedBox(
-            width:16
-          ),
-          Expanded(
-            child: Header(index:index)
-                        )
-                              ],
-                            ),
-                          );
-                        }
-                      }
-            
-           class Header extends StatelessWidget {
-             final index;
-             const Header({ Key? key, this.index }) : super(key: key);
-           
-             @override
-             Widget build(BuildContext context) {
-               return Column( 
-                 crossAxisAlignment: CrossAxisAlignment.start,
-                 children: [
-                   Text("${infoList[index].title}",
-                   style:
-                    TextStyle(
-                      fontSize:32,
-                      fontWeight: FontWeight.w600 ,
-                      color: Colors.white
-                      ),
-                   ),
-                   Padding(
-                     padding: const EdgeInsets.all(8.0),
-                     child: Text("${infoList[index].year} . ${infoList[index].genre}".toUpperCase(),
-                     style: TextStyle(
-                       fontWeight: FontWeight.w600,
-                       color: Colors.amberAccent.shade200
-                     ),
-                     ),
-                   ),
-                   Text.rich(
-                     TextSpan(
-                       style: TextStyle(
-                         fontSize: 12,
-                         fontWeight: FontWeight.w400
-                     ),
-                     children: [
-                       TextSpan(
-                         text:infoList[index].plot,
-                         style: TextStyle(
-                           fontWeight: FontWeight.w400,
-                           fontSize: 14,
-                           color: Colors.grey.shade400
-                         )
-                       ),
-                       
-                       
-                     ]
-                     )),
-
-                   InkWell(
-                     child: Text("More...",
-                         style: TextStyle(color: Colors.amberAccent.shade200,
-                         fontSize: 14
-                         )
-                       ),
-                       onTap: () => {launch(infoList[index].more),
-                       }
-                   )
-                 ],
-                 
-               );
-             }
-           }
-          
-          class Poster extends StatelessWidget {
-            final String poster;
-            const Poster({ Key? key, required this.poster }) : super(key: key);
-          
-            @override
-            Widget build(BuildContext context) {
-              var borderRadius=BorderRadius.all(Radius.circular(10));
-              return Card(
-                child: ClipRRect(
-                   borderRadius: borderRadius=borderRadius,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width/4,
-                    height: 160,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(image: NetworkImage(poster),
-                      fit: BoxFit.cover
-                      )
-                    ),
-                  )
-                  ),
-                  );
-
-            }
-          }
-          class Details extends StatelessWidget {
-            final int index;
-            const Details({ Key? key, required this.index }) : super(key: key);
-          
-            @override
-            Widget build(BuildContext context) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal:18.0),
-                child: Column(
-                   children:[Padding(
-                    padding: const EdgeInsets.all(8.0),
-                  
-                    child: MovieField(field:"Cast",value:infoList[index].actors),
-                  ),
-                   Padding(
-                     padding: const EdgeInsets.all(8.0),
-                     child: MovieField(field:"Director",value:infoList[index].director),
-                   ),
-                   Padding(
-                     padding: const EdgeInsets.all(8.0),
-                     child: MovieField(field:"Awards",value:infoList[index].awards),
-                   )
-                   ]
-                        
-                                  ),
-                                );
-                              }
-                            }
-                  
-                  class MovieField extends StatelessWidget {
-                    final String field;
-                    final String value;
-                    const MovieField({ Key? key, required this.field, required this.value }) : super(key: key);
-                  
-                    @override
-                    Widget build(BuildContext context) {
-                      return Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(child: 
-                          Text("$field : ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                       color: Colors.amberAccent.shade200),
-                          ),
-                          ),
-
-                          Expanded(child: 
-                          Text("$value",
-                         style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                           fontSize: 14,
-                           color: Colors.white
-                         ),
-                          )
-                          )
-                        ],
-                      );
-                    }
-                  }
-                  class Line extends StatelessWidget {
-
-                    const Line({ Key? key }) : super(key: key);
-                  
-                    @override
-                    Widget build(BuildContext context) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 10),
-                        child: Container(
-                          height: 1,
-                          color: Colors.white,
-                          
-                        ),
-                      );
-                    }
-                  }
-
-            class Images extends StatelessWidget {
-              final  List <String> posters ;
-              const Images({ Key? key, required this.posters,  }) : super(key: key);
-            
-              @override
-              Widget build(BuildContext context) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical:8.0,horizontal: 4),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("MORE MOVIE POSTERS...",
-                        style: TextStyle(
-                         fontWeight: FontWeight.w600,
-                         color: Colors.amberAccent.shade200,
-                          fontSize: 14
-                        ),),
-                      ),
-                      
-                      Container(
-                        height:200,
-                        child: ListView.separated(
-                          scrollDirection: Axis.horizontal,
-                           separatorBuilder:(context,index)=>SizedBox(width:8), 
-                           itemCount:posters.length,
-                           itemBuilder: (context,index)=>ClipRRect(
-                             borderRadius:BorderRadius.all(Radius.circular(10)),
-                             
-                               child: Container(
-                                 width: MediaQuery.of(context).size.width/3,
-                                 height: 160,
-                                 decoration: BoxDecoration(
-                                   image: DecorationImage(image: NetworkImage(posters[index]),
-                                   fit: BoxFit.fill
-                                   )
-                                 ),
-                               ),
-                           ),
-                           ),
-                      )
-                    ],
-                    
-                  ),
-                );
-              }
-            }
